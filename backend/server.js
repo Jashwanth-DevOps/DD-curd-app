19ee7b2d103fe52c37ae91da623d9bc27006c165
Ubuntu <ubuntu@ip-172-31-30-20.ec2.internal>
2026-02-24 11:08:05 +0000

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://18.212.97.43"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -31,7 +37,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+
+const baseUrl = '/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
-
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(baseUrl);
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
   }
 
-  get(id: any): Observable<Tutorial> {
-    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
+  get(id: any): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
   }
 
   create(data: any): Observable<any> {
@@ -36,7 +35,7 @@ export class TutorialService {
     return this.http.delete(baseUrl);
   }
 
-  findByTitle(title: any): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
+  findByTitle(title: any): Observable<any> {
+    return this.http.get(`${baseUrl}?title=${title}`);
   }
 }
